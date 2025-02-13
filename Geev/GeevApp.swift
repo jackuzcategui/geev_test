cb854fe1712471ebd78c56f895afe94a7c02c974
Jack Uzcategui <lejackuzcategui@gmail.com>
2025-02-13 11:56:52 +0100

    Initial project setup

---
+++ Geev/GeevApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeevApp.swift
+//  Geev
+//
+//  Created by Jack Uzcategui on 13/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct GeevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
